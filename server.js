e3479df254380e85418d1c89c9ae4d13e8376b30
John Luan <yxl1079@case.edu>
2018-12-30 10:24:00 -0500

    fixed express constructor bug

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var history = require('connect-history-api-fallback')
 var express = require('express');
 var serveStatic = require('serve-static');
 var port = process.env.PORT || 5000;
-app = express.use(serveStatic(__dirname + "/dist"))
+app = express().use(serveStatic(__dirname + "/dist"))
 app.use(history())
 app.listen(port);
 console.log('server started '+ port);
