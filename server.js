f6fb9720227abfdbd0e4dc834bf44c0bf88d95fd
John Luan <yxl1079@case.edu>
2018-12-29 21:34:00 -0500

    adds api fall back for all my routes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+var history = require('connect-history-api-fallback')
 var express = require('express');
-var path = require('path');
 var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
-app.listen(port);
+app = express()
+    .use(serveStatic(__dirname + "/dist"))
+    .use(history)
+    .listen(port);
 console.log('server started '+ port);
