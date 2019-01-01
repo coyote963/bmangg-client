ea898bd2a24f2b6166411b3a7ef510630b047372
John Luan <yxl1079@case.edu>
2018-12-31 22:26:40 -0500

    added elo ratings to each of the badges

--- src/components/Ranks.vue
+++ src/components/Ranks.vue
@@ -11,70 +11,70 @@
       <table class="table">
         <tbody>
           <tr>
-            <td><img src="../assets/badges/0.svg">Earth I</td>
+            <td><img src="../assets/badges/0.svg">Earth I (Below 600)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/1.svg">Earth II</td>
+            <td><img src="../assets/badges/1.svg">Earth II (600-650)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/2.svg">Earth III</td>
+            <td><img src="../assets/badges/2.svg">Earth III (650-700)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/3.svg">Grass I</td>
+            <td><img src="../assets/badges/3.svg">Grass I (700-725)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/4.svg">Grass II</td>
+            <td><img src="../assets/badges/4.svg">Grass II (725-750)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/5.svg">Grass III</td>
+            <td><img src="../assets/badges/5.svg">Grass III (750-775)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/6.svg">Grass IIII</td>
+            <td><img src="../assets/badges/6.svg">Grass IIII (775-800)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/7.svg">Water I</td>
+            <td><img src="../assets/badges/7.svg">Water I (800-833)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/8.svg">Water II</td>
+            <td><img src="../assets/badges/8.svg">Water II (833-866)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/9.svg">Water III</td>
+            <td><img src="../assets/badges/9.svg">Water III (866-900)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/10.svg">Fire I</td>
+            <td><img src="../assets/badges/10.svg">Fire I (900-933)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/11.svg">Fire II</td>
+            <td><img src="../assets/badges/11.svg">Fire II (933-966)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/12.svg">Fire III</td>
+            <td><img src="../assets/badges/12.svg">Fire III (966-1000)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/13.svg">Gold I</td>
+            <td><img src="../assets/badges/13.svg">Gold I (1000-1033)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/14.svg">Gold II</td>
+            <td><img src="../assets/badges/14.svg">Gold II (1033-1066)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/15.svg">Gold III</td>
+            <td><img src="../assets/badges/15.svg">Gold III (1066-1100)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/16.svg">Platinum I</td>
+            <td><img src="../assets/badges/16.svg">Platinum I (1100-1133)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/17.svg">Platinum II</td>
+            <td><img src="../assets/badges/17.svg">Platinum II (1133-1166)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/18.svg">Platinum III</td>
+            <td><img src="../assets/badges/18.svg">Platinum III (1166-1200)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/19.svg">Elite</td>
+            <td><img src="../assets/badges/19.svg">Elite (1200-1250)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/20.svg">Master</td>
+            <td><img src="../assets/badges/20.svg">Master (1250-1300)</td>
           </tr>
           <tr>
-            <td><img src="../assets/badges/21.svg">Grandmaster</td>
+            <td><img src="../assets/badges/21.svg">Grandmaster (1300+)</td>
           </tr>
         </tbody>
       </table>
